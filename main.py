2c9e72f3a45c1fd4974685df597ac93b1fa74746
nvd <navidsoleymani@ymail.com>
2021-04-21 20:09:42 +0430

    keyword extraction by tfidf

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import test
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     test.keyword_extraction_test()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
